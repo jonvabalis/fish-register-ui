aaea17e10028ae949424f62a43089c70b2d17f4e
jonvabalis <tmotiejus@gmail.com>
2025-12-07 02:01:38 +0200

    fix: delete users only for admin

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,20 +1,49 @@
-import { Box } from "@mui/material";
+import { Box, Tab } from "@mui/material";
 import PageHeader from "../components/reusable/PageHeader";
 import UsersTable from "../components/user/UsersTable";
 import ChangeLoginForm from "../components/user/ChangeLoginForm";
+import TabChange from "../components/reusable/TabChange";
+import TabPanel from "../components/reusable/TabPanel";
+import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function User() {
+  const [tabValue, setTabValue] = useState(0);
+  const { role } = useAuth();
+  const isAdmin = role === "admin";
+
+  const handleTabChange = (_e: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+
+  if (!isAdmin) {
+    return (
+      <Box width="100vw">
+        <Box>
+          <PageHeader text="Profilis" />
+        </Box>
+        <Box sx={{ mt: 8 }} />
+        <ChangeLoginForm />
+      </Box>
+    );
+  }
+
   return (
     <Box width="100vw">
       <Box>
         <PageHeader text="Profilis" />
       </Box>
+      <TabChange tabValue={tabValue} handleTabChange={handleTabChange}>
+        <Tab label="Vartotojų sąrašas" />
+        <Tab label="Slaptažodžio keitimas" />
+      </TabChange>
       <Box sx={{ mt: 8 }} />
-      <ChangeLoginForm />
-      <Box sx={{ mt: 8 }} />
-      <PageHeader text="Vartotojų sąrašas" />
-      <Box sx={{ mt: 4 }} />
-      <UsersTable />
+      <TabPanel value={tabValue} index={0}>
+        <UsersTable />
+      </TabPanel>
+      <TabPanel value={tabValue} index={1}>
+        <ChangeLoginForm />
+      </TabPanel>
     </Box>
   );
 }
